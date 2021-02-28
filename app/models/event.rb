ecd62a893cfdc8f7f24945bcecf0b928536d1888
brandonricharda <brandonricharda@gmail.com>
2021-02-27 22:03:17 -0500

    Displays past and upcoming events on User and Event pages

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
     has_many :attendances
     belongs_to :creator, class_name: "User"
     has_many :attendees, through: :attendances, source: :user
+
+    def past
+        self.date < Date.today
+    end
+
+    def upcoming
+        self.date > Date.today
+    end
 end

dc93aea25d8c73b1ca19d973737907475180f5d3
brandonricharda <brandonricharda@gmail.com>
2021-02-28 19:53:33 -0500

    Converts past/upcoming Event methods to scopes and adds internal links throughout application

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,12 +2,6 @@ class Event < ApplicationRecord
     has_many :attendances
     belongs_to :creator, class_name: "User"
     has_many :attendees, through: :attendances, source: :user
-
-    def past
-        self.date < Date.today
-    end
-
-    def upcoming
-        self.date > Date.today
-    end
+    scope :past, -> { where("(date) < ?", Date.today ) }
+    scope :upcoming, -> { where("(date) > ?", Date.today) }
 end

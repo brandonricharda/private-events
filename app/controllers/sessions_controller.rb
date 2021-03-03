bb6edade03b2ec30f259dccffec6e83bb52a5db7
brandonricharda <brandonricharda@gmail.com>
2021-03-03 00:17:53 -0500

    Adds logic that identifies whether logged in user is event creator

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_name(params[:name])
-    if @user
-      session.clear
-      session[:user_id] = @user.id
-      session[:username] = @user.name
-      redirect_to @user, notice: "You have successfully logged in."
+    user = User.find_by_name(params[:name])
+    if user
+      session[:user_id] = user.id
+      session[:username] = user.name
+      redirect_to user, notice: "You have successfully logged in."
     else
       flash.now.alert = "Your login is incorrect."
       render "new"
@@ -16,7 +15,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session.clear
+    session[:user_id] = nil
+    session[:username] = nil
     redirect_to events_path, notice: "You have successfully signed out."
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root "events#index"
+  get "login" => "sessions#new", as: "login"
+  get "logout" => "sessions#destroy", as: "logout"
   resources :users
   resources :sessions
   resources :events

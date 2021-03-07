50bb6f287b7de64c502dc19c52899f7e4fede0cf
brandonricharda <brandonricharda@gmail.com>
2021-03-07 11:23:24 -0500

    Creates infrastructure for Attendances (invitations)

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,2 +1,32 @@
 class AttendancesController < ApplicationController
+    def index
+        @attendances = Attendance.all
+    end
+
+    def new
+        @attendance = Attendance.new
+    end
+
+    def create
+        @attendance = Attendance.new(attendance_params)
+        respond_to do |format|
+            if @attendance.save
+                format.html { redirect_to @attendance, notice: 'Invitation was successfully created.' }
+                format.json { render :show, status: :created, location: @attendance }
+            else
+                format.html { render :new }
+                format.json { render json: @attendance.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+
+    def show
+        @attendance = Attendance.find(params[:id])
+    end
+
+    private
+
+    def attendance_params
+        params.require(:attendance).permit(:event_id, :user_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "events#index"
   get "login" => "sessions#new", as: "login"
   get "logout" => "sessions#destroy", as: "logout"
+  resources :attendances
   resources :users
   resources :sessions
   resources :events
